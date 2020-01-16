bb773dead7f1084812e74fadd1eed79e0ba62a74
Ahmed Shoulah <shoulah@gmail.com>
2020-01-16 23:51:37 +0200

    make_abba

--- Solutions.py
+++ Solutions.py
@@ -3,9 +3,22 @@
 hello_name('Bob') → 'Hello Bob!'
 hello_name('Alice') → 'Hello Alice!'
 hello_name('X') → 'Hello X!' """
-
+""" 
 def hello_name(name):
     return "Hello " + name + "!"
 
 
 print(hello_name('Bob'))
+
+ """
+
+"""  Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+make_abba('Hi', 'Bye') → 'HiByeByeHi'
+make_abba('Yo', 'Alice') → 'YoAliceAliceYo'
+make_abba('What', 'Up') → 'WhatUpUpWhat' """
+
+def make_abba(a, b):
+    return a + b + b + a
+
+print(make_abba("Hi", "Bye"))    
